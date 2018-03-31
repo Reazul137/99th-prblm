bacafa9b1b8ead97fcca57afb515f9db43cf6a63
Reazul Islam <reazulislam137@gmail.com>
2018-04-01 01:15:50 +0600

    first commit

---
+++ april-01-03.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"\n\n Print the sum of teo number : \n";
+    cout<<"___________________________________\n";
+    cout<<"The sum of 29 and 30 is : "<<29+30<<"\n\n";\
+
+    return 0;
+}
+
